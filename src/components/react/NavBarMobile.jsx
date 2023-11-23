8530f499e8e904160d1eb717d8c8535fffd90b00
Diegoo11 <ynoacamino@gmail.com>
2023-11-22 19:49:10 -0500

    add Header and Title

---
+++ src/components/react/NavBarMobile.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import MoreOptionsIcon from './MoreOptionsIcon.jsx';
+import ButtonLink from './ButtonLink.jsx';
+import ExitButton from './ExitButton.jsx';
+
+export default function NavBarMobile() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button type="button" aria-label="más opciones" className="w-9 md:hidden" onClick={handleClick}>
+        <MoreOptionsIcon />
+      </button>
+      <nav className={`flex flex-col items-center justify-center gap-4 fixed h-screen
+        w-screen bg-one top-0 left-0 ${isOpen || 'left-[-100vh] opacity-0'} md:hidden transition-all`}
+      >
+        <ButtonLink>Trabajos</ButtonLink>
+        <ButtonLink>Proyectos</ButtonLink>
+        <ButtonLink>Tecnologias</ButtonLink>
+        <ButtonLink>Cerfiticados</ButtonLink>
+        <button type="button" aria-label="salir" className="w-9 mt-20" onClick={handleClick}>
+          <ExitButton />
+        </button>
+      </nav>
+    </>
+  );
+}
