15cf3914e1be4680b7133b8ca709713bcbc4d9b1
Diegoo11 <ynoacamino@gmail.com>
2023-11-27 11:07:24 -0500

    add projects

--- src/components/react/PopoverIcon.jsx
+++ src/components/react/PopoverIcon.jsx
@@ -2,15 +2,15 @@ import {
   Popover, PopoverTrigger, PopoverContent,
 } from '@nextui-org/react';
 
-export default function Card({ s }) {
+export default function PopoverIcon({ s }) {
   return (
     <Popover placement="top">
       <PopoverTrigger className="hover:cursor-pointer">
-        <img src={`/icons/${s}.svg`} alt={s} className="w-14 h-14 p-1 hover:bg-gray-200 transition-all rounded-xl" />
+        <img src={`/icons/${s}.svg`} alt={s} className="w-12 h-12 md:w-14 md:h-14 p-1 hover:bg-gray-300 transition-all rounded-xl" />
       </PopoverTrigger>
       <PopoverContent>
         <div className="px-1 py-2">
-          <div className="text-xl font-bold">{s}</div>
+          <div className="text-xl font-semibold">{s}</div>
         </div>
       </PopoverContent>
     </Popover>
