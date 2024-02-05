73c5f70e6cbaddd92188dd252768209d0c2a3f46
Diegoo11 <ynoacamino@gmail.com>
2024-02-04 20:50:47 -0500

    add Frontend Developer certificate

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -29,4 +29,10 @@ export const certificates = [
     logo: '/icons/MongoDB.svg',
     url: 'https://ti-user-certificates.s3.amazonaws.com/ae62dcd7-abdc-4e90-a570-83eccba49043/248f5b57-d69c-55a8-b06c-f9fbf6a5555e-elza-pallo-cb8a213b-56f2-5c65-a6cc-e577ba981986-certificate.pdf',
   },
+  {
+    name: 'Frontend Developer',
+    img: '/certificates/frontEnd.png',
+    logo: '/icons/React.svg',
+    url: 'https://www.hackerrank.com/certificates/04f8d88ca646',
+  },
 ];
